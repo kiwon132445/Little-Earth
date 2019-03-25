9ac42376761c7075ce06d4d031d98ab2b6f62ec2
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-03-25 16:40:17 -0700

    Added lifespan descrease function, sign out button.

--- JS/update_lifespan.js
+++ JS/update_lifespan.js
@@ -1,7 +1,10 @@
 (function(){
-  function changeLifespan(width) {
+  
+  const lifespan = document.getElementById("lifespan");
+  var lifespanDecreaseId;
+  
+  function displayLifespan(width) {
 
-    var lifespan = document.getElementById("lifespan");
     if (width > 97) {
       lifespan.style.borderRadius = '20px';
     }
@@ -10,18 +13,50 @@
     var lifespanText = document.getElementById("lifespanText");
     lifespanText.innerHTML = width + ',000 years';
   }
+  
+  function startLifespanDecrease(){
+    lifespanDecreaseId = setInterval(decreaseLifespanBar, 1000); //1% in 1sec;
+    console.log("lifespanDecreaseId: " + lifespanDecreaseId);
+  }
+  
+  function stopLifespanDecrease(){
+    clearInterval(lifespanDecreaseId);
+  }
+  
+  function decreaseLifespanBar(){
+    let currentWidth = lifespan.style.width.split("%")[0];
+    console.log("currentWidth: " + currentWidth);
+//    displayLifespan(currentWidth - 1);
+    writeLifespanToDB(currentWidth - 1); //should it be written every second or in the very end of the user session?
+  }
+  
+  function writeLifespanToDB(width){
+    writeToDB({"lifespan" : width});
+  }
+  
+  // Parameter updatesForCurrentUser accepts an object, with key-value pairs,
+  // where key is a key in DB under "users/userUID" node, 
+  // and value - the new value to write.
+  function writeToDB(updatesForCurrentUser){
+    firebase.auth().onAuthStateChanged(function (user) {
+      if(user){
+        firebase.database().ref("users/" + user.uid).update(updatesForCurrentUser);
+      }
+  });
+  }
 
   firebase.auth().onAuthStateChanged(function(user) {
     if (user){
         var currentUid = user.uid;
-        console.log(currentUid);
+        console.log("currentUid: " + currentUid);
 
         var dbRef = firebase.database().ref().child("users/" + currentUid + "/lifespan"); 
         dbRef.on(
           'value',
           function(snapshot) {
-          changeLifespan(snapshot.val());
+          displayLifespan(snapshot.val());
         });
+      startLifespanDecrease();
     }
   });
   
