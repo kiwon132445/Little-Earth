8f62691e4eee102e971490c920f3194cc1f9366e
Kateryna Bozhenko <kateryna.bozhenko@gmail.com>
2019-03-29 12:26:46 -0700

    Added functions: restartGame, decreaseLifespanBar, increaseLifespanBar

--- JS/create_user.js
+++ JS/create_user.js
@@ -1,23 +1,40 @@
-(function () {
+
+const startScoreValues =
+{
+  "lifespan": 50,
+  "experience": 0
+};
+
+function initializeUser() {
   firebase.auth().onAuthStateChanged(function (user) {
-    if (user){
+    if (user) {
       var userRef = firebase.database().ref("users").child(user.uid);
-      var promise = userRef.once("value", function(snap){
-        if (snap.val() === null){
+      var promise = userRef.once("value", function (snap) {
+        if (snap.val() === null) {
           userRef.update({
-            "name" : user.displayName,
-            "email" : user.email,
-            "lifespan" : 50,
-            "experience" : 0
+            "name": user.displayName,
+            "email": user.email,
+            "lifespan": 50,
+            "experience": 0
           });
         }
       });
-      promise.then(function(){
+      promise.then(function () {
         document.getElementById("userName").append(user.displayName + "!");
       });
     }
   });
-  
-    
-  
-})();
+}
+
+function restartGame() {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var userRef = firebase.database().ref("users").child(user.uid);
+      userRef.once("value", function (snap) {
+        userRef.update(startScoreValues);
+      }).then(startLifespanDecrease);
+    }
+  });
+}
+
+initializeUser();
--- JS/update_lifespan.js
+++ JS/update_lifespan.js
@@ -1,59 +1,93 @@
-(function(){
-  
-  const lifespan = document.getElementById("lifespan");
-  var lifespanDecreaseId;
-  
-  function startLifespanDecrease(){
-    lifespanDecreaseId = setInterval(decreaseLifespanBar, 10000); //1% in 10sec;
-  }
-  
-  function stopLifespanDecrease(){
-    clearInterval(lifespanDecreaseId);
-  }
-  
-  function displayLifespan(width) {
+const lifespan = document.getElementById("lifespan");
+var lifespanDecreaseId;
 
-    if (width > 97) {
-      lifespan.style.borderRadius = '20px';
-    }
-    lifespan.style.width = width + '%';
+(function () {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      var currentUid = user.uid;
 
-    var lifespanText = document.getElementById("lifespanText");
-    lifespanText.innerHTML = width + ',000 years';
-  }
-  
-  function decreaseLifespanBar(){
-    let currentWidth = lifespan.style.width.split("%")[0];
-    writeLifespanToDB(currentWidth - 1); //should it be written every second or in the very end of the user session?
-  }
-  
-  function writeLifespanToDB(width){
-    writeToDB({"lifespan" : width});
-  }
-  
-  // Parameter updatesForCurrentUser accepts an object, with key-value pairs,
-  // where key is a key in DB under "users/userUID" node, 
-  // and value - the new value to write.
-  function writeToDB(updatesForCurrentUser){
-    firebase.auth().onAuthStateChanged(function (user) {
-      if(user){
-        firebase.database().ref("users/" + user.uid).update(updatesForCurrentUser);
-      }
-    });
-  }
-
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user){
-        var currentUid = user.uid;
-      
-        var dbRef = firebase.database().ref().child("users/" + currentUid + "/lifespan"); 
-        dbRef.on(
-          'value',
-          function(snapshot) {
+      var dbRef = firebase.database().ref().child("users/" + currentUid + "/lifespan");
+      dbRef.on(
+        'value',
+        function (snapshot) {
           displayLifespan(snapshot.val());
         });
       startLifespanDecrease();
     }
   });
-
 })();
+
+function startLifespanDecrease() {
+  lifespanDecreaseId = setInterval(decreaseLifespanBar, 1000); //1% in 1sec;
+}
+
+function stopLifespanDecrease() {
+  clearInterval(lifespanDecreaseId);
+}
+
+function displayLifespan(width) {
+
+  if (width > 97) {
+    lifespan.style.borderRadius = '20px';
+  }
+  lifespan.style.width = width + '%';
+
+  var lifespanText = document.getElementById("lifespanText");
+  lifespanText.innerHTML = width + ',000 years';
+}
+
+function getCurrentLifespanWidth(){
+  return parseInt(lifespan.style.width.split("%")[0]);
+}
+
+function decreaseLifespanBar(amount = 1) {
+  let currentWidth = getCurrentLifespanWidth();
+  if ((currentWidth - amount) <= 0) {
+    //Game over!
+    writeLifespanToDB(0);
+    endGame(false);
+  } else {
+    writeLifespanToDB(currentWidth - amount);
+  }
+}
+
+function increaseLifespanBar(amount = 25) {
+  let currentWidth = getCurrentLifespanWidth();
+  if ((currentWidth + amount) > 100) {
+    writeLifespanToDB(100);
+  } else {
+    writeLifespanToDB(currentWidth + amount);
+  }
+}
+
+function endGame(didWin) {
+  stopLifespanDecrease();
+  if (didWin) {
+    displayWinScreen();
+  } else {
+    displayLoseScreen();
+  }
+}
+
+function displayWinScreen() {
+  console.log("You won!");
+}
+
+function displayLoseScreen() {
+  console.log("You lost!");
+}
+
+function writeLifespanToDB(width) {
+  writeToDB({ "lifespan": width });
+}
+
+// Parameter updatesForCurrentUser accepts an object, with key-value pairs,
+// where key is a key in DB under "users/userUID" node, 
+// and value - the new value to write.
+function writeToDB(updatesForCurrentUser) {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      firebase.database().ref("users/" + user.uid).update(updatesForCurrentUser);
+    }
+  });
+}
