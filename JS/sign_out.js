e1e4faa3efe031eaa7b8cebf94404339a2c6b6d8
kiwon132445 <kiwon132445@gmail.com>
2019-04-04 14:09:47 -0700

    water minigame v1.3

--- JS/sign_out.js
+++ JS/sign_out.js
@@ -2,7 +2,7 @@
   document.getElementById("signOutBtn").addEventListener("click", function(e){
     var promise = firebase.auth().signOut();
     promise.then(function(){
-      window.location.href="login.html";
+      window.location.href="index.html";
     });
   });
 })();
